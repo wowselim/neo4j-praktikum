ba9d0e37fab52cd20b526d1276fd29b9720d7234
Selim Dincer <wowselim@live.de>
2017-12-20 11:39:01 +0100

    use local db instead of embedded one

--- src/main/java/NeoMain.java
+++ src/main/java/NeoMain.java
@@ -3,48 +3,17 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class NeoMain {
-    enum Country implements Label {
-        JAPAN, GERMANY
-    }
-
-    enum CarType implements Label {
-        COUPE, WAGON, SUV
-    }
-
-    enum ServiceTypes implements RelationshipType {
-        PRODUCES, REPAIRS
-    }
-
     public static void main(String[] args) throws IOException {
-        File storeDir = new File("db.graphdb");
-        removeDir(storeDir);
+        // z.B. "C:/neo4jDatabases/graph.db" (mit Quotes!)
+        File storeDir = new File(args[0]);
         GraphDatabaseService db = new GraphDatabaseFactory().newEmbeddedDatabase(storeDir);
 
-        try (Transaction t = db.beginTx()) {
-            Node honda = db.createNode();
-            honda.setProperty("name", "Honda");
-            honda.addLabel(Country.JAPAN);
-
-            Node s2000 = db.createNode();
-            s2000.setProperty("name", "Honda S2000");
-            s2000.addLabel(CarType.COUPE);
-
-            Relationship relationship = honda.createRelationshipTo(
-                    s2000, ServiceTypes.PRODUCES
-            );
-            relationship.setProperty("since", 1999);
-
-            t.success();
-        }
-
-        try(Transaction t = db.beginTx();
-        ResourceIterator<Node> cars = db.findNodes(CarType.COUPE)) {
-            while(cars.hasNext()) {
-                Node car = cars.next();
+        try (Transaction t = db.beginTx();
+             ResourceIterator<Node> people = db.findNodes(Label.label("Person"))) {
+            while (people.hasNext()) {
+                Node car = people.next();
                 System.out.println(car.getProperty("name"));
             }
 
@@ -52,16 +21,5 @@ public class NeoMain {
         }
 
         db.shutdown();
-    }
-
-    private static void removeDir(File f) {
-        try {
-            Files.walk(f.toPath())
-                    .filter(Files::isRegularFile)
-                    .map(Path::toFile)
-                    .forEach(File::delete);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
